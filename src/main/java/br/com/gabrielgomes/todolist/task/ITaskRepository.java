8a5eb09fded362888d9a3e38b37fa205a8bad54e
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-10-13 01:40:11 -0300

    feat: create tasks

---
+++ src/main/java/br/com/gabrielgomes/todolist/task/ITaskRepository.java
@@ -0,0 +1,13 @@
+package br.com.gabrielgomes.todolist.task;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+
+    List<TaskModel> findByIdUser(UUID idUser);
+
+}
